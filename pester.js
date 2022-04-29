4c4a4c0af827d04b2c1b1dfe7326185cb12c267e
Luke <rugulous1999@hotmail.com>
2022-04-29 15:34:18 +0100

    Make request, getting API token

--- pester.js
+++ pester.js
@@ -1,10 +1,10 @@
-const OSM = require('./lib/OSM')(require('./config'));
+const OSM = require('./lib/OSM')(require('./config'), 'section:attendance:read+section:member:read');
 
 const today = '2022-04-28'; //TODO get curr date
 
 
 async function main(){
-	const res = await OSM.doRequest('oauth/token');
+	const res = await OSM.getResource('oauth/resource');
 	console.log(res);
 }
 

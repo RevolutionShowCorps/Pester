94f0a64b962143b88668a6dc5147a36ab548af4a
Luke <rugulous1999@hotmail.com>
2022-04-29 15:35:37 +0100

    Tidied up OSM import

--- pester.js
+++ pester.js
@@ -1,7 +1,7 @@
-const OSM = require('./lib/OSM')(require('./config'), 'section:attendance:read+section:member:read');
+const config = require('./config');
+const OSM = require('./lib/OSM')(config, 'section:attendance:read+section:member:read'); //TODO do something fancy with scopes
 
 const today = '2022-04-28'; //TODO get curr date
-
 
 async function main(){
 	const res = await OSM.getResource('oauth/resource');

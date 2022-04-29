3f46be4201b6be8e872d81097204e5b137a6b480
Luke <rugulous1999@hotmail.com>
2022-04-29 15:57:46 +0100

    Get sections

--- pester.js
+++ pester.js
@@ -4,7 +4,7 @@ const OSM = require('./lib/OSM')(config, 'section:attendance:read+section:member
 const today = '2022-04-28'; //TODO get curr date
 
 async function main(){
-	const res = await OSM.getResource('oauth/resource');
+	const res = await OSM.getSections();
 	console.log(res);
 }
 

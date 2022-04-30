ff5cf34711db3067354a51b3fd3318e8ab87ead0
Luke <rugulous1999@hotmail.com>
2022-04-30 11:52:02 +0100

    OSM class retain section/term IDs

--- pester.js
+++ pester.js
@@ -1,11 +1,18 @@
 const config = require('./config');
 const OSM = require('./lib/OSM')(config, 'section:attendance:read+section:member:read'); //TODO do something fancy with scopes
 
+const section_name = 'Queensbury Scout Band';
+
 const today = '2022-04-28'; //TODO get curr date
 
 async function main(){
-	const res = await OSM.getSections();
-	console.log(res);
+	const sections = await OSM.getSections();
+
+	if(!OSM.initialised()){
+		const section = sections.filter(s => s.section_name.toLowerCase().trim() === section_name.toLowerCase().trim())[0];
+		OSM.setTermFromSection(section, Date.now());
+	}
+
 }
 
 main();

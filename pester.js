a4740cf8698af943b2735e232c162f832daf86d6
Luke <rugulous1999@hotmail.com>
2022-04-30 12:41:48 +0100

    App skeleton

--- pester.js
+++ pester.js
@@ -1,24 +1,56 @@
 const config = require('./config');
-const OSM = require('./lib/OSM')(config, 'section:attendance:read+section:member:read'); //TODO do something fancy with scopes
+const OSM = require('./lib/OSM')(config, 'section:attendance:read+section:member:read+section:programme:read'); //TODO do something fancy with scopes
 
 const section_name = 'Queensbury Scout Band';
 
-const today = '2022-04-28'; //TODO get curr date
+const today = new Date('2022-04-28'); //TODO get curr date
 
 async function main(){
-	const sections = await OSM.getSections();
+	//initialises section and term for us!
+	await OSM.getSections();
+	await checkForMeeting();
+}
 
-	if(!OSM.initialised()){
-		const section = sections.filter(s => s.section_name.toLowerCase().trim() === section_name.toLowerCase().trim())[0];
-		if(!OSM.setTermFromSection(section, Date.now())){
-			console.error("Could not find suitable section and term - please check you have set up OSM correctly and are passing in the correct OAuth values");
-			return -1;
+//step 1
+//can run at some point early morning
+//finds if there's a meeting today, and when it is
+//schedules the attendance check
+async function checkForMeeting(){
+	const meetings = await OSM.getMeetings(today);
+
+	const dateOnly = new Date(today.getTime());
+	dateOnly.setHours(0, 0, 0, 0);
+	
+	meetings.forEach(async m => {
+		const meetingDate = new Date(m.meetingdate);
+		meetingDate.setHours(0, 0, 0, 0);
+
+		if(meetingDate.getTime() == dateOnly.getTime()){
+			console.log(`Found meeting for ${m.meetingdate}, starting at ${m.starttime}`);
+			await scheduleRegisterCheck(meetingDate, m.starttime, m.endTime);
 		}
-	}
+	})
+}
 
-	const members = await OSM.getMemberSummary();
-	console.log(members);
+//step 2
+//schedule tasks (steps 3 and 4)
+async function scheduleRegisterCheck(date, startTime, endTime){
+	await contactMissingParents(date);
+
+	await updateRegisters(date);
+}
+
+//step 3
+//the scheduled task
+async function contactMissingParents(date){
 
 }
+
+//step 4
+//tidyup
+//mark questionables as not attended
+async function updateRegisters(date){
+
+} 
 
 main();
